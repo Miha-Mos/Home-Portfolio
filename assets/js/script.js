471239d980520469be6e84ed9cec8f5da1311ed8
Miha-Mos <71607457+Miha-Mos@users.noreply.github.com>
2020-12-08 22:13:36 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,17 +45,32 @@ $(function(){
 
 // Счётчик цифр
 
-$('.part8-num').each(function(){
-  $(this).prop('Counter', 0).animate({
-    Counter:$(this).text()
-  },{
-    duration: 3000,
-    easing: 'swing',
-    step:function(now){
-      $(this).text(Math.ceil(now));
-    }
-  });
-});
+var show = true;
+    var countbox = ".part8";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+         
+          $('.part8-num').each(function(){
+            $(this).prop('Counter', 0).animate({
+             Counter:$(this).text()
+          },{
+           duration: 3000,
+           easing: 'swing',
+           step:function(now){
+           $(this).text(Math.ceil(now));
+            }
+          });
+         });
+
+          show = false;
+        }
+    });
 
 // Видео на странице
 
